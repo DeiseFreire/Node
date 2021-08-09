75428be49ac0eb86acf7f7667cb3ed98d8101f57
Deise Freire <51007898+DeiseFreire@users.noreply.github.com>
2021-08-09 19:23:15 -0300

    Protocolo HTTP no Node.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-/** 
- * LIMA, Victor.Curso de node.js: Como Instalar o Node.js #02. 28 nov. 2018, 06 dec.  
- * 2018. 63 p. Notas de Aula. No prelo.
+/**
+ * LIMA, Victor. Curso de node.js: Protocolo HTTP #05. 20 nov. 2018, 06 dec. 2018. 63 p.  * Notas de Aula. No prelo.
  */
 
-console.log("Hello World!")
+var http = require('http');
+http.create.Server(function( /**req, */ res) {
+    res.end("Hello world! Welcome my website");
+}).listen(8081);
+console.log("O Servidor está rodando!")
